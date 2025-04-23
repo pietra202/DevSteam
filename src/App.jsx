aa5e93a9dadaad7c54c96249d913c3a4468b4050
Pietra <pgodoifelipe@gmail.com>
2025-04-23 11:58:26 -0300

    finalização projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Header from './Components/Header'
+import Header from "./components/Header";
+import Promotion from "./components/Promotion";
+import CarrinhoOffCanvas from "./Components/CarrinhoOffCanvas";
+
 
 function App() {
+  const [carrinhoItem, setCarrinhoItem] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("devcarrinho", JSON.stringify(carrinhoItem));
+  }, [carrinhoItem]);
+
+  useEffect(() => {
+    const salvaCarrinho = localStorage.getItem("devcarrinho");
+    salvaCarrinho && setCarrinhoItem(JSON.parse(salvaCarrinho));
+  }, []);
+
+  // console.log(localStorage.getItem("devcarrinho"));
+
+  const handleAddCarrinho = (produto) => {
+    setCarrinhoItem((itemAnterior) => {
+      const existe = itemAnterior.find((item) => item.id === produto.id);
+      if (existe) {
+        return itemAnterior.map((item) =>
+          item.id === produto.id
+            ? { ...item, quantidade: item.quantidade + 1 }
+            : item
+        );
+      } else {
+        return [...itemAnterior, { ...produto, quantidade: 1 }];
+      }
+    });
+  };
+
   return (
     <>
-      <Header />
+      <Header contadorJogos={carrinhoItem.length} />
+      <Promotion
+        onAddCarrinho={handleAddCarrinho} //adicionando o click para promoção
+        <CarrinhoOffCanvas carrinhoItem={carrinhoItem}
+      />
     </>
   );
 }
